363103d452a13ae021e1028b05400e0897a21d04
thenhz <thenhz@gmail.com>
2020-01-11 08:48:52 +0000

    it works...need to include twiddle code

--- src/PID.cpp
+++ src/PID.cpp
@@ -17,9 +17,9 @@ PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_)
 {
-this->Kp = Kp;
-	this->Ki = Ki;
-	this->Kd = Kd;
+this->Kp = Kp_;
+	this->Ki = Ki_;
+	this->Kd = Kd_;
 
 	p_error = 0;
 	d_error = 0;
@@ -28,7 +28,7 @@ this->Kp = Kp;
 
 void PID::UpdateError(double cte)
 {
-d_error = cte - p_error;
+	d_error = cte - p_error;
 	p_error = cte;
 	i_error += cte;
 
